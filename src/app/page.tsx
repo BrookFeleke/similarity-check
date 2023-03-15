975fa39349860ce0ee97410b52730243c321a6f3
Brook <felekebrook05@gmail.com>
2023-03-15 18:19:13 +0300

    changed folder structure and add import aliases

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import Paragraph from '@/ui/Paragraph'
+import LargeHeading from '@/ui/LargeHeading'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className='bg-amber-400 text-lg'>
-     Hello World
+    <main className='text-lg'>
+     <LargeHeading className='bg-slate-600'>This is a large heading</LargeHeading>
+     <Paragraph className='bg-slate-400'>Hello There</Paragraph>
     </main>
   )
 }
