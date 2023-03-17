15aaf6f56911d6e101e1403cb10297b197d65524
Brook <felekebrook05@gmail.com>
2023-03-17 13:44:47 +0300

    added reusable components and did nav bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,8 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main className='text-lg'>
-     <LargeHeading className='bg-slate-600'>This is a large heading</LargeHeading>
-     <Paragraph className='bg-slate-400'>Hello There</Paragraph>
+     {/* <LargeHeading className='bg-slate-600'>This is a large heading</LargeHeading>
+     <Paragraph className='bg-slate-400'>Hello There</Paragraph> */}
     </main>
   )
 }
